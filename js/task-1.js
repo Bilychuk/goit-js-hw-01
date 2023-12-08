11f3d511e1e0a92af38b87ce167843093f5a577b
Bilychuk <bilychukkateryna@ukr.net>
2023-12-08 15:27:10 +0200

    corrections

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,8 @@
-'use strict'
+'use strict';
 function makeTransaction(quantity, pricePerDroid) {
-    let totalPrice = quantity * pricePerDroid;
-    return `"You ordered ${quantity} droids worth ${totalPrice} credits!"`;
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
-
-makeTransaction(5, 3000);
-makeTransaction(3, 1000);
-makeTransaction(10, 500);
 
 console.log(makeTransaction(5, 3000));
 console.log(makeTransaction(3, 1000));
--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,8 @@
-'use strict'
+'use strict';
 function getShippingMessage(country, price, deliveryFee) {
-    let totalPrice = price + deliveryFee;
-    return `"Shipping to ${country} will cost ${totalPrice} credits"`;
+    const totalPrice = price + deliveryFee;
+    return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
-
-getShippingMessage("Australia", 120, 50);
-getShippingMessage("Germany", 80, 20);
-getShippingMessage("Sweden", 100, 20);
 
 console.log(getShippingMessage("Australia", 120, 50)); 
 console.log(getShippingMessage("Germany", 80, 20));
